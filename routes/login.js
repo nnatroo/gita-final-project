d7a1587756db066c24e5c57ab0b0654308584719
nnatroo <nugonatroshvili@gmail.com>
2025-04-15 21:27:39 +0400

    Add Login Page

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  if (req.session.user) {
-    return res.redirect('/dashboard');
-  }
-  res.render('login', { error: null });
+    if (req.session.user) {
+        return res.redirect('/dashboard');
+    }
+    res.render('login', { error: null });
 });
 
 module.exports = router;
