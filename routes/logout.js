67066f44f4dc4df49a799f82e302c8029d7ec9e5
iraklilt <iraklilt@gmail.com>
2025-04-22 22:02:02 +0400

    Add session creating and bcrypt logic

--- routes/logout.js
+++ routes/logout.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    if (req.session.user) {
-        return res.redirect('/blogs');
-    }
-    res.redirect('login');
+    req.session.destroy(function (err) {
+        res.redirect('/');
+    })
 });
 
 module.exports = router;
