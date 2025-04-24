906ef2b5ec22a4c44327137c563916ce78b4c2a2
iraklilt <iraklilt@gmail.com>
2025-04-24 21:55:08 +0400

    Add blog posting functionality

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const fs = require("fs")
+
+const BLOGS_FILE = "blogs.json";
 
 const requireAuth = (req, res, next) => {
     if (req.session.user) {
         next();
     } else {
+        console.log(req.session.user);
         res.redirect('/login');
     }
 }
 
 router.get('/', requireAuth, function (req, res, next) {
-    res.render('blogs', {error: null});
+    const data = fs.readFileSync(BLOGS_FILE)
+    const blogs = JSON.parse(data)
+
+
+    res.render('blogs', {error: null, blogs});
+
 });
+
+router.get('/new', requireAuth, function (req, res, next) {
+    res.render('new_blog', {error: null});
+});
+
+router.post('/new', requireAuth, function (req, res, next) {
+    const {title, content} = req.body;
+    if (!title || !content) {
+        res.render('new_blog', {error: `Missing title or content`});
+    }
+
+    const data = fs.readFileSync(BLOGS_FILE)
+    const blogs = JSON.parse(data)
+
+    const newBlog = {
+        id: String(Date.now()),
+        title,
+        content,
+        author: req.session.user.email,
+        date: new Date().toLocaleString()
+    }
+    blogs.push(newBlog);
+    fs.writeFileSync(BLOGS_FILE, JSON.stringify(blogs, null, 2));
+
+    res.redirect("/blogs");
+})
 
 module.exports = router;
